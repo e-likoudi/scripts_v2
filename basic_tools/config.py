e1d97d27dd171c7ebb375195b94b7f0513df8315
e-likoudi <111080471+e-likoudi@users.noreply.github.com>
2026-03-11 12:51:02 +0200

    Refactor config.py paths for improved clarity

--- basic_tools/config.py
+++ basic_tools/config.py
@@ -1,11 +1,11 @@
-BOOKS_PATH = "/Users/chara/Documents/thesis/scripts_v2/medic_data/"
-MODEL = "nomic-embed-text"
-CHROMA_PATH = f"./{MODEL}_db"
-SUMMARIES_FILE = "/Users/chara/Documents/thesis/scripts_v2/medic_data/summaries.txt"
+BOOKS_PATH = ""  #path for the folder with the papers
+MODEL = "nomic-embed-text"  #embedding model
+CHROMA_PATH = f"./{MODEL}_db"  #path to save chromadb
+SUMMARIES_FILE = "/summaries.txt"  #path to save the summaries.txt
 
-PROTOCOL_MODEL = "llama3.1:latest" 
-PROTOCOL_FILE = f"/Users/chara/Documents/thesis/scripts_v2/medic_data/protocol_{PROTOCOL_MODEL}.txt"
+PROTOCOL_MODEL = "llama3.1:latest"  #base model for data extraction from the paper 
+PROTOCOL_FILE = f"/protocol_{PROTOCOL_MODEL}.txt"  #path to save the differentiation steps and other details 
 
-BOOK_FOR_QA = "yang2008"
-QUESTION = "What is the main finding of the paper?"
+BOOK_FOR_QA = ""  #name of the paper we want to use for asking a question
+QUESTION = "What is the main finding of the paper?"  #sample question
 
